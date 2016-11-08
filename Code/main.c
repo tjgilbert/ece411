0a3b0bbab33a390cb5f0e76b955d7f42c79c54b7
Tyler <tgil2@pdx.edu>
2016-11-08 10:04:25 -0800

    changed pwm mode

--- Code/main.c
+++ Code/main.c
@@ -18,7 +18,7 @@ void initIO(void) {
  //Initial setup:
  // MCUCR = 0x40; // Uncomment to disable internal pull ups
     DDRA =  0b11111111; //Port A data direction register [DDA7:DDA0] (0=input 1=output)
-    DDRB =  0b00000000; //Port B data direction register [DDB3:DDB0] (0=input 1=output)
+    DDRB =  0xFF; //Port B data direction register [DDB3:DDB0] (0=input 1=output)
     PORTA = 0b00000000; //Port A data register [PORTA7:PORTA0]
     PORTB = 0b00000000; //Port B data register [PORTA3:PORTB0]
     
@@ -27,26 +27,24 @@ void initIO(void) {
     
     OSCCAL = 0x7F; // set internal oscilator frequency. The two values overlap, ie. 0x7F > 0x80
     clock_prescale_set(0);  // default is /8.. (0) = /1 , (1)= /2, (8)=/256
-    // PLLCSR &= ~_BV(PCKE); //Set Synchronous mode (disable PLL)   on attiny85??
-    DDRA |= 0b0000000;  // PA5, PA6, PA7, PB2 can be configured as PWM output.
-    TCCR0B |= 7;// Waveform Generation Mode Bit Description. Mode 7=Fast PWM, Counts up to OCR0A.
-    OCR0A = 0; //this is the fastest.. like 460kHz fast
     
-    //GTCCR |= _BV(COM1B1) | _BV(COM1B0); //Only generate output on the standard not the inverted pin
+    //DDRA|=(1<<PA6);
     
-  /*
-
+    TCCR1A = _BV(COM1A1) | _BV(WGM11);     //TCCR1A = COM1A1 COM1A0 COM1B1 COM1B0 – – WGM11 WGM10
    
+    TCCR1B = _BV(CS10) | _BV(WGM12) | _BV(WGM13);
+        ICR1 = 80;
+        OCR1A = 40;
+        OCR1B = 40;
     
-    OCR1C = 200;    //Governs the period of PWM  (count to this value)
-    OCR1B  = 50;  // Duty Cycle (should be about half of OCR1C)
     
-    TCCR1  = _BV(CS10); //No Prescaler. //Since the PLL is not enabled this should be in ref. to cpu clock.
     
-    TCCR1 |= _BV(COM1A1) | _BV(COM1A0);  // needed on ATTINY 85
-
-}
-     */
+    TCCR0A = _BV(COM0B1) /*| _BV(COM0B0)*/ | _BV(WGM02)|_BV(WGM01) | _BV(WGM00);
+    
+    TCCR0B = _BV(CS00) ; //No prescaling - toggle mode?
+    
+    OCR0A =10;
+    OCR0B = 50;
 }
 
 int main(void) {
@@ -54,14 +52,9 @@ int main(void) {
 	initIO();
 
     
-    
-    
 	while (1) {
 	/*
-        PORTA = 0b11111111; //Port A data register [PORTA7:PORTA0]
-    _delay_ms(1);
-        PORTA = 0b00000000; //Port A data register [PORTA7:PORTA0]
-        _delay_ms(1);
+        do nothing
     */
      }
 	return 0; // never reached
